3e23adb21f6eb91af29a85800393ec68e02f0fc0
Dat Lv <lvoddat95@yahoo.com>
2021-05-24 16:49:59 +0700

    Update master.blade.php

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -64,48 +64,48 @@
 <!-- Footer -->
 
 <!-- CORE JS-->
-<script src="{{ asset('themes/app-assets/vendors/js/jquery-3.3.1.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/jquery-ui.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/bootstrap/bootstrap.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/perfect-scrollbar.jquery.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/moment/moment-with-locales.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/jquery.tipsy.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/sweetalert2.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/jquery.sticky-sidebar.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/isotope.pkgd.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/jquery.fancybox.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/jQuery.print.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/hc-offcanvas-nav.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/jquery-3.3.1.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/jquery-ui.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/bootstrap/bootstrap.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/perfect-scrollbar.jquery.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/moment/moment-with-locales.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/jquery.tipsy.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/sweetalert2.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/jquery.sticky-sidebar.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/isotope.pkgd.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/jquery.fancybox.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/jQuery.print.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/hc-offcanvas-nav.js') }}"></script>
 <!-- /CORE JS-->
 
 <!-- JS Trang-->
-<script src="{{ asset('themes/app-assets/vendors/js/select2.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/cleave.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/jquery.repeater.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/jquery.cloner.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/form/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/datatables.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/buttons.flash.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/jszip.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/pdfmake.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/vfs_fonts.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/buttons.html5.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/datatable/buttons.print.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/chart/d3.min.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/chart/d3_tooltip.js') }}"></script>
-<script src="{{ asset('themes/app-assets/vendors/js/chart/c3.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/select2.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/cleave.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/jquery.repeater.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/jquery.cloner.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/form/jquery.validate.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/datatables.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/dataTables.responsive.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/dataTables.buttons.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/buttons.flash.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/jszip.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/pdfmake.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/vfs_fonts.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/buttons.html5.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/datatable/buttons.print.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/chart/d3.min.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/chart/d3_tooltip.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/vendors/js/chart/c3.min.js') }}"></script>
 <!-- /JS Trang-->
 
-<script src="{{ asset('themes/app-assets/js/i18n.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/js/i18n.js') }}"></script>
 
 @yield('javascript')
 
 <!-- Custom JS-->
-<script src="{{ asset('themes/app-assets/js/app.js') }}"></script>
-<script src="{{ asset('themes/app-assets/js/acts.js') }}"></script>
-<script src="{{ asset('themes/app-assets/js/script_custom.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/js/app.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/js/acts.js') }}"></script>
+<script defer src="{{ asset('themes/app-assets/js/script_custom.js') }}"></script>
 <!-- /Custom JS-->
 
 
